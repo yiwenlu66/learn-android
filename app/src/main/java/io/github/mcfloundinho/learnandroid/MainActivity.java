866b38411fa47ac0cc83d8edf19c01de130e4342
mcfloundinho <mcfloundinho@gmail.com>
2016-02-17 15:09:46 +0800

    add text watcher

--- app/src/main/java/io/github/mcfloundinho/learnandroid/MainActivity.java
+++ app/src/main/java/io/github/mcfloundinho/learnandroid/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final EditText edit_username = (EditText) findViewById(R.id.edit_username);
+        final EditText edit_password = (EditText) findViewById(R.id.edit_password);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        edit_username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                edit_password.setText(edit_username.getText());
+                textView.setText(edit_username.getText());
+            }
+        });
+
    }
 
     @Override
